e70d903ad2e05045d82f7ad3f0f7d9ed34099078
Yean Seyha <yeanseyha7@gmail.com>
2021-03-08 10:00:51 +0700

    upload is working fine.[bug] mongodb error

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
+const indexRouter = require('./routes/index')
+
+const bookRouter = require('./routes/books')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://user1:010700923@cluster0.xdvle.mongodb.net/Book?retryWrites=true&w=majority')
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+
+app.use('/books', bookRouter)
+
+app.listen(3000)
