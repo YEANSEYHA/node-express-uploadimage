03e8cd8b80a2f5eedf4628feba650da86d198e2a
YEAN SEHA <yeanseyha7@gmail.com>
2022-12-14 15:39:08 +0700

    add readme file

---
+++ readme.md
@@ -0,0 +1,11 @@
+# To run project
+
+npm install or yarn install
+
+# To start project
+
+npm run dev
+
+# Add env monngodb
+
+server.js
--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
 
-const bookRouter = require('./routes/books')
+const bookRouter = require('./routes/books');
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://user1:010700923@cluster0.xdvle.mongodb.net/Book?retryWrites=true&w=majority')
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://upload_image:upload_image@cluster0.xdvle.mongodb.net/?retryWrites=true&w=majority');
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
 
-app.use('/', indexRouter)
+app.use('/', indexRouter);
 
-app.use('/books', bookRouter)
+app.use('/books', bookRouter);
 
-app.listen(3000)
+app.listen(3000);
